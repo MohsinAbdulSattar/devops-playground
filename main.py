efd993041246fd4ce8ab82a1de479f2b289714c3
Mohsin <mohsinabdulsattar7@gmail.com>
2025-08-12 23:37:38 +0500

    updated main.py script for day 11

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print ("Hello from Github Actions!")
-print ("Workflow test!")
+# main main.py
+html_content = """
+<html>
+  <head>
+    <title>Python Output</title>
+  </head>
+  <body>
+    <h1>Hello from GitHub Actions!</h1>
+    <p>Workflow test!</p>
+  </body>
+</html>
+"""
+
+with open("output.html", "w") as f:
+    f.write(html_content)
+
+print("HTML file generated: output.html")
